f9dfc6fa6994228191f02d261dc8ca4b74fcb856
Dant3s15 <82174072+Dant3s15@users.noreply.github.com>
2021-08-17 12:19:01 +0200

    added active status for buttons & custom field js

--- dist/app/js/calculator.js
+++ dist/app/js/calculator.js
@@ -1,33 +1,47 @@
 let bill = document.getElementById('billfield');
 let numberOfPeople = document.getElementById('numberofpeople');
-
-// let btn1 = document.getElementById('10');
-// let btn2 = document.getElementById('15');
-// let btn3 = document.getElementById('25');
-// let btn4 = document.getElementById('50');
 var buttons = [0, 
 document.getElementById('5'),
 document.getElementById('10'),
 document.getElementById('15'),
 document.getElementById('25'),
-document.getElementById('50')]
+document.getElementById('50'),
+document.getElementById('custom')]
 
 let tipamount = document.getElementById('tipamount');
 let totalTip = document.getElementById('totalresult');
 
-
-
 function returnResult(bill, numberOfPeople, percent, buttonsArr){
+    var getClass = document.getElementsByClassName('activebtn');
     
+    for(var i = 0; i < getClass.length; i++)
+    {
+        getClass[i].classList.remove('activebtn');
+        
+    }
+
+    getBtn = document.getElementById(percent.id);
+    getHover = document.getElementsByClassName('btn:hover');
+    // alert(getHover);
+    getBtn.classList.add('activebtn');
     bill = bill.value;
     numberOfPeople = numberOfPeople.value;
-    percent = buttons[buttonsArr].id;
-    tipamount.innerHTML = (bill/numberOfPeople)*percent/100;
-    totalTip.innerHTML = ((bill/numberOfPeople)*percent/100)*numberOfPeople;//WIP - probably wrong
+
+    if (buttonsArr == 6){
+        percent = buttons[6].value;
+    }
+    else percent = buttons[buttonsArr].id;
+   
+
+    let tipResult = tipamount.innerHTML = Math.round(((bill / numberOfPeople) * percent / 100) * 100) / 100;
+    totaltip = totalTip.innerHTML = Math.round(((bill / numberOfPeople) + tipResult) * 100) / 100;
 }
 
-for(let i = 1; i<=5;i++){
-    buttons[i].addEventListener('click',function(){returnResult(bill, numberOfPeople, buttons[i].id, i)})
-
+for(let i = 1; i<=6;i++){
+    buttons[i].addEventListener('click',function(){returnResult(bill, numberOfPeople, buttons[i], i)})
 }
 
+// function updateInput(ish){
+//     document.getElementById("numberofpeople").value = ish;
+// }
+// onchange=updateInput(this.value);
